d905f86e243fdbfd33d900ddeb48aea877ce0f0f
Konstantin <kbogatyrev@gmail.com>
2024-06-19 17:33:41 -0700

    Progress checkin.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,157 +1,198 @@
-const Declinables = ['Noun', 'LongAdj', 'Pronoun', 'PronounAdj', 'Numeral', 'NumeralAdj', 
-                     'PartPresAct', 'PartPresPassLong', 'PartPastAct', 'PartPastPassLong', 
-                     'PartPastPassShort'];
+const Declinables = [
+  "Noun",
+  "LongAdj",
+  "Pronoun",
+  "PronounAdj",
+  "Numeral",
+  "NumeralAdj",
+  "PartPresAct",
+  "PartPresPassLong",
+  "PartPastAct",
+  "PartPastPassLong",
+  "PartPastPassShort",
+];
 
-const SingularPlural = ['Noun', 'LongAdj', 'ShortAdj', 'Pronoun', 'PronounAdj', 'PresentTense', 
-                        'Imperative', 'PastPresAct', 'PartPresPassLong', 'PartPresPassShort', 
-                        'PartPastAct', 'PartPastPassLong', 'PartPastPassShort', 'NumeralAdj'];
+const SingularPlural = [
+  "Noun",
+  "LongAdj",
+  "ShortAdj",
+  "Pronoun",
+  "PronounAdj",
+  "PresentTense",
+  "Imperative",
+  "PastPresAct",
+  "PartPresPassLong",
+  "PartPresPassShort",
+  "PartPastAct",
+  "PartPastPassLong",
+  "PartPastPassShort",
+  "NumeralAdj",
+];
 
-const ThreeGenders = ['LongAdj', 'ShortAdj', 'PronounAdj', 'PastTense', 'PartPresAct', 'PartPresPassLong', 
-                      'PartPresPassShort', 'PartPastAct', 'PartPastPassLong', 'PartPastPassShort', 'NumeralAdj'];
+const ThreeGenders = [
+  "LongAdj",
+  "ShortAdj",
+  "PronounAdj",
+  "PastTense",
+  "PartPresAct",
+  "PartPresPassLong",
+  "PartPresPassShort",
+  "PartPastAct",
+  "PartPastPassLong",
+  "PartPastPassShort",
+  "NumeralAdj",
+];
 
-const AnimRelevant = ['LongAdj', 'PronounAdj', 'PartPresAct', 'PartPresPassLong', 'PartPastAct', 
-                      'PartPastPassLong', 'NumeralAdj'];
+const AnimRelevant = [
+  "LongAdj",
+  "PronounAdj",
+  "PartPresAct",
+  "PartPresPassLong",
+  "PartPastAct",
+  "PartPastPassLong",
+  "NumeralAdj",
+];
 
 function setDbPath(path) {
-    obj.setDbPath(path);
+  obj.setDbPath(path);
 }
 
 function wordQuery(searchString, response) {
+  obj.clear();
 
-    obj.clear();
+  var lexData = {
+    lexemes: [],
+  };
 
-    var lexData = {
-        lexemes:[]
-    };
+  class Lexeme {}
+  class Inflection {}
 
-    class Lexeme {};
-    class Inflection {};
+  response.writeHead(200, { "Content-Type": "text/json; charset=utf-8" });
 
-    response.writeHead(200, { "Content-Type": "text/json; charset=utf-8" });
-
-    try {
-        var bLexemeFound = obj.getLexemesByInitialForm(searchString);
-        if (!bLexemeFound) {
-            console.log("Lexeme %s not found.", searchString);
-            response.write("Lexeme not found");
-            response.end();
-            return;
-        }
-
-        var bLexemeLoaded = obj.loadFirstLexeme();
-        if (!bLexemeLoaded) {
-            console.error("loadFirstLexeme() failed");
-            response.write("Internal error");
-            response.end();
-            return;
-        }
-        
-        do {
-            lexeme = new Lexeme();
-//            lexeme.lexemeId = obj.getLexemeProperty("lexemeId");
-            lexeme.sourceForm = obj.getLexemeProperty("sourceForm");
-            lexeme.mainSymbol = obj.getLexemeProperty("mainSymbol");
-            lexeme.partOfSpeech = obj.getLexemeProperty("partOfSpeech");
-            if ("Verb" == lexeme.partOfSpeech) {
-                lexeme.isTransitive = obj.getLexemeProperty("isTransitive");
-            }
-            spryazhSm = obj.getLexemeProperty("spryazhSm");
-            if (spryazhSm) {
-                lexeme.spryazhSmRef = obj.getLexemeProperty("spryazhSmRef");
-            }
-
-            section = obj.getLexemeProperty("section");
-            if (section >= 1) {
-                lexeme.section = obj.getLexemeProperty("section");
-            }
-            
-            var bInflectionLoaded = obj.loadFirstInflection();
-            if (!bInflectionLoaded) {
-                console.error("loadFirstInflection() failed");
-                response.write("Internal error");
-                response.end();
-                return;
-            }
-
-            var inflectionId = obj.getInflectionProperty("inflectionId");
-            var bGenerated = obj.generateParadigm(inflectionId);
-            if (!bGenerated) {
-                console.error("Failed to generate paradigm for inflection ID %s.", inflectionId);
-                response.write("Failed to generate paradigm.");
-                response.end();
-                return;
-            }
-    
-            lexeme.inflections = [];
-            do {
-                inflection = new Inflection();
-                inflection.inflectionId = obj.getInflectionProperty("inflectionId");
-                inflection.inflectionType = obj.getInflectionProperty("inflectionType");
-                const ap1 = obj.getInflectionProperty("accentType1");
-                if (ap1 != "") {
-                    inflection.accentType1 = ap1;
-                    const ap2 = obj.getInflectionProperty("accentType2");
-                    if (ap2 != "") {
-                        inflection.accentType2 = ap2;
-                    }
-                }
-                if ("Verb" == lexeme.partOfSpeech) {
-                    aspectPair = obj.getInflectionProperty("aspectPair");
-                    if (aspectPair.length > 0) {
-                        inflection.aspectPair = aspectPair;
-                    }
-                }
-                lexeme.inflections.push(inflection);
-
-            } while(obj.loadNextInflection());
-
-            lexData.lexemes.push(lexeme);
-
-//            import ("uuid");
-
-        } while (obj.loadNextLexeme());
-    }
-    catch (e) {
-        console.error("NodeJS exception: %s", e.message);
+  try {
+    var bLexemeFound = obj.getLexemesByInitialForm(searchString);
+    if (!bLexemeFound) {
+      console.log("Lexeme %s not found.", searchString);
+      response.write("Lexeme not found");
+      response.end();
+      return;
     }
 
-    var json = JSON.stringify(lexData.lexemes);
-    console.log(JSON.parse(json));
-    response.write(json);
-   
-    response.end();
+    var bLexemeLoaded = obj.loadFirstLexeme();
+    if (!bLexemeLoaded) {
+      console.error("loadFirstLexeme() failed");
+      response.write("Internal error");
+      response.end();
+      return;
+    }
 
-}   // wordQuery()
+    do {
+      lexeme = new Lexeme();
+      //            lexeme.lexemeId = obj.getLexemeProperty("lexemeId");
+      lexeme.sourceForm = obj.getLexemeProperty("sourceForm");
+      lexeme.mainSymbol = obj.getLexemeProperty("mainSymbol");
+      lexeme.partOfSpeech = obj.getLexemeProperty("partOfSpeech");
+      if ("Verb" == lexeme.partOfSpeech) {
+        lexeme.isTransitive = obj.getLexemeProperty("isTransitive");
+      }
+      spryazhSm = obj.getLexemeProperty("spryazhSm");
+      if (spryazhSm) {
+        lexeme.spryazhSmRef = obj.getLexemeProperty("spryazhSmRef");
+      }
+
+      section = obj.getLexemeProperty("section");
+      if (section >= 1) {
+        lexeme.section = obj.getLexemeProperty("section");
+      }
+
+      var bInflectionLoaded = obj.loadFirstInflection();
+      if (!bInflectionLoaded) {
+        console.error("loadFirstInflection() failed");
+        response.write("Internal error");
+        response.end();
+        return;
+      }
+
+      //      var inflectionId = obj.getInflectionProperty("inflectionId");
+      //      var bGenerated = obj.generateParadigm(inflectionId);
+      //      if (!bGenerated) {
+      //        console.error(
+      //          "Failed to generate paradigm for inflection ID %s.",
+      //          inflectionId
+      //        );
+      //        response.write("Failed to generate paradigm.");
+      //        response.end();
+      //        return;
+      //      }
+
+      lexeme.inflections = [];
+      do {
+        inflection = new Inflection();
+        inflection.inflectionId = obj.getInflectionProperty("inflectionId");
+        inflection.inflectionType = obj.getInflectionProperty("inflectionType");
+        const ap1 = obj.getInflectionProperty("accentType1");
+        if (ap1 != "") {
+          inflection.accentType1 = ap1;
+          const ap2 = obj.getInflectionProperty("accentType2");
+          if (ap2 != "") {
+            inflection.accentType2 = ap2;
+          }
+        }
+        if ("Verb" == lexeme.partOfSpeech) {
+          aspectPair = obj.getInflectionProperty("aspectPair");
+          if (aspectPair.length > 0) {
+            inflection.aspectPair = aspectPair;
+          }
+        }
+        lexeme.inflections.push(inflection);
+      } while (obj.loadNextInflection());
+
+      lexData.lexemes.push(lexeme);
+
+      //            import ("uuid");
+    } while (obj.loadNextLexeme());
+  } catch (e) {
+    console.error("NodeJS exception: %s", e.message);
+  }
+
+  var json = JSON.stringify(lexData.lexemes);
+  console.log(JSON.parse(json));
+  response.write(json);
+  response.end();
+} // wordQuery()
 
 function paradigmQuery(inflectionId, response) {
-    var paradigm = {
-        wordForms : []
-    };
+  var paradigm = {
+    wordForms: [],
+  };
 
-    response.writeHead(200, { "Content-Type": "text/json; charset=utf-8" });
+  response.writeHead(200, { "Content-Type": "text/json; charset=utf-8" });
 
-    try {
-        var bGenerated = obj.generateParadigm(inflectionId);
-        if (!bGenerated) {
-            console.error("Failed to generate paradigm for inflection ID %s.", inflectionId);
-            response.write("Failed to generate paradigm.");
-            response.end();
-            return;
-        }
+  try {
+    console.log("typeof = " + typeof inflectionId);
+    var bGenerated = obj.generateParadigm(inflectionId);
+    if (!bGenerated) {
+      console.error(
+        "Failed to generate paradigm for inflection ID %s.",
+        inflectionId
+      );
+      response.write("Failed to generate paradigm.");
+      response.end();
+      return;
+    }
+    var bWordFormLoaded = obj.loadFirstWordForm(inflectionId);
+    if (!bWordFormLoaded) {
+      console.error("loadFirstWordForm() failed.");
+      response.write("Internal error.");
+      response.end();
+      return;
+    }
 
-        var bWordFormLoaded = obj.loadFirstWordForm();
-        if (!bWordFormLoaded) {
-            console.error("loadFirstWordForm() failed.");
-            response.write("Internal error.");
-            response.end();
-            return;
-        }
+    do {
+      let subParadigm = obj.getWordFormProperty("subParadigm");
 
-        do {
-            let subParadigm = obj.getWordFormProperty("subParadigm");
-
-            var wordForm = {
-                wordForm : '',
+      var wordForm = {
+        wordForm: "",
         //        stem : '',
         //        ending : '',
         //        partOfSpeech : '',
@@ -169,132 +210,138 @@ function paradigmQuery(inflectionId, response) {
         //        isDifficult : false
         //        leadComent : '',
         //        trailingComment : ''
-            };
-        
-            wordForm.wordForm = obj.getWordFormProperty("wordForm");
+      };
 
-            if (Declinables.includes(subParadigm)) {
-                wordForm.case = obj.getWordFormProperty("case");
-            }
+      wordForm.wordForm = obj.getWordFormProperty("wordForm");
 
-            if (SingularPlural.includes(subParadigm)) {
-                wordForm.number = obj.getWordFormProperty("number");
-            }
+      if (Declinables.includes(subParadigm)) {
+        wordForm.case = obj.getWordFormProperty("case");
+      }
 
-            if (ThreeGenders.includes(subParadigm)) {
-                wordForm.gender = obj.getWordFormProperty("gender");
-            }
+      if (SingularPlural.includes(subParadigm)) {
+        wordForm.number = obj.getWordFormProperty("number");
+      }
 
-            if ('PresentTense' === subParadigm) {
-                wordForm.person = obj.getWordFormProperty("person");
-            }
+      if (ThreeGenders.includes(subParadigm)) {
+        wordForm.gender = obj.getWordFormProperty("gender");
+      }
 
-            if (AnimRelevant.includes(subParadigm)) {
-                if (Declinables.includes(subParadigm)) {
-                    if ('Accusative' == wordForm.case && wordForm.gender != 'Neuter') {
-                        wordForm.animacy = obj.getWordFormProperty("animacy");
-                    }
-                } else {
-                    wordForm.animacy = obj.getWordFormProperty("animacy");      // when will this happen??
-                }
-            }
-            
-//            wordForm.reflexivity = obj.getWordFormProperty("reflexivity");
-//            wordForm.aspect = obj.getWordFormProperty("aspect");
-            let status = obj.getWordFormProperty('status');
-            if (status && status !== 'Common') {
-                wordForm.status = status;
-            }
-            let isIrregular = obj.getWordFormProperty('isIrregular');
-            if (isIrregular && isIrregular !== '') {
-                wordForm.isIrregular = isIrregular;
-            }
-//            let isVariant = obj.getWordFormProperty('isVariant');
-//            if (isVariant && isVariant != '') {
-//                wordForm.isVariant = isVariant;
-//            }
-            let isDifficult = obj.getWordFormProperty('isDifficult');
-            if (isDifficult && isDifficult !== '') {
-                wordForm.isDifficult = isDifficult;
-            }
-//            wordForm.leadComent = obj.getWordFormProperty("leadComment");
-//            wordForm.trailingComment = obj.getWordFormProperty("trailingComment");
+      if ("PresentTense" === subParadigm) {
+        wordForm.person = obj.getWordFormProperty("person");
+      }
 
-            paradigm.wordForms.push(JSON.parse(JSON.stringify(wordForm)));
-            
-        } while (obj.loadNextWordForm());
+      if (AnimRelevant.includes(subParadigm)) {
+        if (Declinables.includes(subParadigm)) {
+          if ("Accusative" == wordForm.case && wordForm.gender != "Neuter") {
+            wordForm.animacy = obj.getWordFormProperty("animacy");
+          }
+        } else {
+          wordForm.animacy = obj.getWordFormProperty("animacy"); // when will this happen??
+        }
+      }
 
-        var json = JSON.stringify(paradigm);
-        console.log(JSON.parse(json));
-        response.write(json);               
-        response.end();
-    }
-    catch (e) {
-        console.error("NodeJS exception: %s", e.message);
-    }
-}   // paradigmQuery()
+      //            wordForm.reflexivity = obj.getWordFormProperty("reflexivity");
+      //            wordForm.aspect = obj.getWordFormProperty("aspect");
+      let status = obj.getWordFormProperty("status");
+      if (status && status !== "Common") {
+        wordForm.status = status;
+      }
+      let isIrregular = obj.getWordFormProperty("isIrregular");
+      if (isIrregular && isIrregular !== "") {
+        wordForm.isIrregular = isIrregular;
+      }
+      //            let isVariant = obj.getWordFormProperty('isVariant');
+      //            if (isVariant && isVariant != '') {
+      //                wordForm.isVariant = isVariant;
+      //            }
+      let isDifficult = obj.getWordFormProperty("isDifficult");
+      if (isDifficult && isDifficult !== "") {
+        wordForm.isDifficult = isDifficult;
+      }
+      //            wordForm.leadComent = obj.getWordFormProperty("leadComment");
+      //            wordForm.trailingComment = obj.getWordFormProperty("trailingComment");
+
+      paradigm.wordForms.push(JSON.parse(JSON.stringify(wordForm)));
+    } while (obj.loadNextWordForm(inflectionId));
+
+    var json = JSON.stringify(paradigm);
+    console.log(JSON.parse(json));
+    response.write(json);
+    response.end();
+  } catch (e) {
+    console.error("NodeJS exception: %s", e.message);
+  }
+} // paradigmQuery()
 
 function wordParse(response) {
-    console.log("Request handler 'wordParse' was called.");
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("============================== wordParse");
-    response.end();
+  console.log("Request handler 'wordParse' was called.");
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write("============================== wordParse");
+  response.end();
 }
 
-function Word(incompleteParse, lineBreak, segmentId, seqNum, wordSource, gramHash) {
-    this.incompleteParse = incompleteParse;
-    this.lineBreak = lineBreak;
-    this.segmentId = segmentId;
-    this.seqNum = seqNum;
-    this.wordSource = wordSource;
-    this.gramHashes = gramHash;
+function Word(
+  incompleteParse,
+  lineBreak,
+  segmentId,
+  seqNum,
+  wordSource,
+  gramHash
+) {
+  this.incompleteParse = incompleteParse;
+  this.lineBreak = lineBreak;
+  this.segmentId = segmentId;
+  this.seqNum = seqNum;
+  this.wordSource = wordSource;
+  this.gramHashes = gramHash;
 }
 
 function formatSegment(segNum, segObj, text) {
-    try {
-        for (let at = 0; at < segObj.segmentSize(segNum); ++at) {
-            const word = new Word(segObj.getWordInTextProperty(segNum, at, 'incompleteParse'), 
-                                  segObj.getWordInTextProperty(segNum, at, 'lineBreak'), 
-                                  segObj.getWordInTextProperty(segNum, at, 'segmentId'), 
-                                  segObj.getWordInTextProperty(segNum, at, 'seqNum'), 
-                                  segObj.getWordInTextProperty(segNum, at, 'wordSource'), 
-                                  segObj.getWordInTextProperty(segNum, at, 'gramHash'));
-            text.push(word);
-        }
+  try {
+    for (let at = 0; at < segObj.segmentSize(segNum); ++at) {
+      const word = new Word(
+        segObj.getWordInTextProperty(segNum, at, "incompleteParse"),
+        segObj.getWordInTextProperty(segNum, at, "lineBreak"),
+        segObj.getWordInTextProperty(segNum, at, "segmentId"),
+        segObj.getWordInTextProperty(segNum, at, "seqNum"),
+        segObj.getWordInTextProperty(segNum, at, "wordSource"),
+        segObj.getWordInTextProperty(segNum, at, "gramHash")
+      );
+      text.push(word);
     }
-    catch (e) {
-        console.log("NodeJS exception : " + e.message);
-    }
+  } catch (e) {
+    console.log("NodeJS exception : " + e.message);
+  }
 }
 
 // arguments: (1) 1st segment id, (2) num of segments to show
 // e.g., HTTP GET /text&startIdx=0&size=2
 function textQuery(start_id, size, response) {
-    let text = [];
-    try {
-        var rc = obj.loadFirstSegment(start_id);
-        formatSegment(0, obj, text);
+  let text = [];
+  try {
+    var rc = obj.loadFirstSegment(start_id);
+    formatSegment(0, obj, text);
     //    console.log(text);
 
-        for (let segNum = 1; segNum < size; ++segNum) {
-            bRet = obj.loadNextSegment();
-            if (!bRet) {
-                break;
-            }
-            formatSegment(segNum, obj, text)
-        }
-    } catch (e) {
-        console.log ("NodeJS exception: " + e.message);
+    for (let segNum = 1; segNum < size; ++segNum) {
+      bRet = obj.loadNextSegment();
+      if (!bRet) {
+        break;
+      }
+      formatSegment(segNum, obj, text);
     }
+  } catch (e) {
+    console.log("NodeJS exception: " + e.message);
+  }
 
-    response.writeHead(200, { "Content-Type": "text/json; charset=utf-8" });
-    var json = JSON.stringify(text);
-//    console.log(JSON.parse(json));
-    response.write(json);
-    response.end();
+  response.writeHead(200, { "Content-Type": "text/json; charset=utf-8" });
+  var json = JSON.stringify(text);
+  //    console.log(JSON.parse(json));
+  response.write(json);
+  response.end();
 }
 
-const addon = require('bindings')('zal-web.node');
+const addon = require("bindings")("zal-web.node");
 const obj = new addon.ZalWeb();
 
 //const sDbPath = "/home/konstantin/Zal-Web/data/ZalData_Master.db3";
--- router.js
+++ router.js
@@ -1,40 +1,40 @@
 //    https://www.zalizniak.com/query?word=баба
 
 function route(handle, pathname, params, response) {
-    console.log("About to route request for " + pathname);
+  console.log("About to route request for " + pathname);
 
-    if (typeof handle[pathname] !== 'function') {
-        console.log("No request handler found for " + pathname);
-        response.writeHead(404, { "Content-Type": "text/plain" });
-        response.write("404 Not found");
-        response.end();
-        return;
-    }
+  if (typeof handle[pathname] !== "function") {
+    console.log("No request handler found for " + pathname);
+    response.writeHead(404, { "Content-Type": "text/plain" });
+    response.write("404 Not found");
+    response.end();
+    return;
+  }
 
-    const searchParams = new URLSearchParams(params);
+  const searchParams = new URLSearchParams(params);
 
-    switch(pathname) {
-        case '/query':
-            const word = searchParams.get('word');
-            console.log('Word search: ' + word);
-            handle[pathname](word, response);
-            break;
-        case '/forms':
-            const inflectionId = parseInt(searchParams.get('inflection-id'), 10);
-            handle[pathname](inflectionId, response);
-            break;
-        case '/parse':
-            break;
-        case '/text':
-            const start_idx = parseInt(searchParams.get('start-idx'), 10);
-            const size = parseInt(searchParams.get('size'), 10);
-            handle[pathname](start_idx, size, response);
-            break;
-        default:
-            console.log("No request handler found for " + pathname);
-            response.writeHead(404, { "Content-Type": "text/plain" });
-            response.write("404 Not found");
-            response.end();    
-    }
+  switch (pathname) {
+    case "/query":
+      const word = searchParams.get("word");
+      console.log("Word search: " + word);
+      handle[pathname](word, response);
+      break;
+    case "/forms":
+      const inflectionId = parseInt(searchParams.get("inflection-id"), 10);
+      handle[pathname](inflectionId, response);
+      break;
+    case "/parse":
+      break;
+    case "/text":
+      const start_idx = parseInt(searchParams.get("start-idx"), 10);
+      const size = parseInt(searchParams.get("size"), 10);
+      handle[pathname](start_idx, size, response);
+      break;
+    default:
+      console.log("No request handler found for " + pathname);
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.write("404 Not found");
+      response.end();
+  }
 }
 exports.route = route;
